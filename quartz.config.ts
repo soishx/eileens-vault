55b38fdd8daf65d9442ccf1036f8329aaab33142
soishx <gao20080724@gmail.com>
2025-08-21 20:45:27 +0800

    Quartz sync: Aug 21, 2025, 8:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Private"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
