3914c27ce0adc5f98eaafdd2f7e14739c5f8276c
soishx <gao20080724@gmail.com>
2025-08-23 22:57:38 +0800

    Quartz sync: Aug 23, 2025, 10:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -36,8 +36,8 @@ const config: QuartzConfig = {
           dark: "#2b2b2b",
           secondary: "#9882eb",
           tertiary: "#84a59d",
-          highlight: "rgb(152, 130, 235)",
-          textHighlight: "#1850a488",
+          highlight: "rgba(255, 208, 0, 0.4)",
+          textHighlight: "#6f95ce88",
         },
         darkMode: {
           light: "#161618",
